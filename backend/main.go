58aa6056ab5d78ed181e46e424c3b5731504b10c
Lou Foster <lf6f@virginia.edu>
2019-03-28 15:39:12 -0400

    uploads working. tested on sizes up to 7G

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
@@ -31,7 +34,77 @@ func healthCheckHandler(c *gin.Context) {
 
 // submitHandler
 func submitHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, "Submitted")
+
+	// fmt.Println(header.Filename)
+	// log.Printf("Received request to upload %s; create temp file", filename)
+	// out, err := os.Create("./uploads/" + filename)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer out.Close()
+	// log.Printf("Receiving file")
+	// _, err = io.Copy(out, file)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Printf("DONEReceiving file")
+	// c.String(http.StatusOK, "Submitted")
+
+	/**
+		WITH CHNKING, LOOKS LIKE THIS (other form params):
+		dzuuid: 57030696-46bf-4aa6-8575-9693ef9d7896
+	dzchunkindex: 318
+	dztotalfilesize: 637439656
+	dzchunksize:     2000000
+	dztotalchunkcount: 319
+	dzchunkbyteoffset: 636000000
+	*/
+	// file, err := c.FormFile("file")
+	// if err != nil {
+	// 	c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+	// 	return
+	// }
+
+	chunkIdx := c.PostForm("dzchunkindex")
+	if chunkIdx != "" {
+		// this is a chunked file; open in append mode and write to it
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("Unable to get form file: %s", err.Error()))
+			return
+		}
+		filename := header.Filename
+		log.Printf("Received CHUNKED request to upload %s, chunk %s size %s", filename, chunkIdx, c.PostForm("dzchunksize"))
+		dest := fmt.Sprintf("./uploads/%s", filename)
+		outFile, err := os.OpenFile(dest, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("unable to receive file %s", err.Error()))
+		}
+		defer outFile.Close()
+		_, err = io.Copy(outFile, file)
+	} else {
+		// not chunked; just save the file in the temp dir
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+		filename := filepath.Base(file.Filename)
+		dest := fmt.Sprintf("./uploads/%s", filename)
+		log.Printf("Receiving non-chunked file %s", filename)
+		if err := c.SaveUploadedFile(file, dest); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+		log.Printf("Done receiving %s", filename)
+		c.String(http.StatusOK, "Submitted")
+	}
+
+}
+
+// testHandler
+func testHandler(c *gin.Context) {
+	c.String(http.StatusOK, "TEST PASSED")
 }
 
 /**
@@ -57,6 +130,7 @@ func main() {
 	api := router.Group("/api")
 	{
 		api.POST("/submit", submitHandler)
+		api.GET("/test", testHandler)
 	}
 	router.Use(static.Serve("/", static.LocalFile("./public", true)))
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,90 @@
 <template>
   <div class="home">
     <h2>Archives Records Transfer Form</h2>
+    <vue-dropzone :useCustomSlot=true id="customdropzone" :options="dropzoneOptions">
+      <div class="dropzone-custom">
+        <div class="upload title">Drag and drop to upload content</div>
+        <div class="upload subtitle">or click to select a file from your computer</div>
+      </div>
+    </vue-dropzone>
   </div>
 </template>
 
 <script>
-
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data: function () {
+    return {
+      dropzoneOptions: {
+        url: '/api/submit',
+        createImageThumbnails: false,
+        // timeout: null,    no tmimeouts
+        // addRemoveLinks: true,
+        maxFilesize: null,
+        chunking: true,
+        chunkSize: 10000000, // bytes = 10Mb,
+        previewTemplate: this.template()
+      }
+    }
+  },
+  methods: {
+    template: function () {
+        return `<div class="dz-preview dz-file-preview" style="width:100px; margin:  5px;">
+          <style type="text/css">
+          .custom-remove { z-index:1000;position:absolute;right:5px;top:5px;opacity:0.8;}
+          .custom-remove:hover { opacity:1;cursor:pointer; }
+          .dz-progress.custom { margin-top:0 !important; top: auto !important; bottom:10px !important; border-radius:0 !important;background:white !important;}
+          .dz-upload.custom { background: darkslateblue !important}
+          .truncated {display:block;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
+          p.failure {text-align:center; padding: 1px 4px 0 4px; background: white; color: #a00;
+                      border-left: 4px solid rgba(33,150,243,.8);
+                      border-right: 4px solid rgba(33,150,243,.8);padding: 2px 0 0 0;}
+          </style>
+                <img class="custom-remove" src="remove.png" data-dz-remove/>
+                <div class="dz-image">
+                    <div data-dz-thumbnail-bg></div>
+                </div>
+                <div class="dz-details" style="text-align: center;padding: 30px 10px;">
+                    <div class="dz-name" style="font-size:12px"><span class="truncated" data-dz-name></span></div>
+                    <div class="dz-size" style="font-size:12px"><span data-dz-size></span></div>
+                </div>
+                <div class="dz-progress custom"><span class="dz-upload custom" data-dz-uploadprogress></span></div>
+                <div class="dz-error-message"><span data-dz-errormessage></span></div>
+                <div class="dz-success-mark"><i class="fa fa-check"></i></div>
+                <div class="dz-error-mark" style="top:60%"><p class="failure">FAILED<p></i></div>
+            </div>
+        `;
+      },
+  }
 }
 </script>
+
 <style scoped>
+
 div.home {
   padding: 15px 25px;
+  min-height: 600px;
 }
 h2 {
   margin: 0;
   color:#51822F;
   font-weight: 500;
+}
+div.dropzone-custom {
+  color: #666;
+}
+div.upload.title {
+  font-weight: bold;
+  font-size: 1.5em;
+  margin-top: 15px;
+}
+div.upload.subtitle {
+  font-weight: 500;
+  margin-top: 5px;
 }
 </style>
