c3c2ff1dee11e84424ef848dddfdc47696c903c3
Lou Foster <lf6f@virginia.edu>
2019-04-01 10:46:26 -0400

    added content to home page and adjust layout

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,51 @@
 <template>
    <div class="home">
       <h2>
-         <span>Archives Records Transfer System</span>
+         <span>University Archives Records Transfer Form</span>
          <span class="admin">
             <router-link to="/admin">
                <button class="pure-button pure-button-primary">Admin Access</button>
             </router-link>
          </span>
       </h2>
+      <div class="contact">
+         <p>University Archives</p>
+         <p>Albert & Shirley Small Special Collections Library</p>
+         <p>P.O. Box 400110</p>
+         <p>Charlottesville, VA, 22904-4110</p>
+         <table>
+            <tr><td class="label">Contact:</td><td>Bethany Anderson, University Archivist</td></tr>
+            <tr><td class="label">Phone:</td><td>(434) 982-2980</td></tr>
+            <tr><td class="label">Email:</td><td><a href="mailto:bga3d@virginia.edu">bga3d@virginia.edu</a></td></tr>
+         </table>
+      </div>
+      <div class="info">
+         <p>
+            Donors or their representatives should complete this form when transferring paper-based 
+            and/or digital administrative records to the University Archives. When possible, 
+            send this form (and a box/folder title list for records over 1 cubic foot) electronically 
+            prior to sending records. We will then advise you on the appropriate transfer method. 
+         </p>
+         <p>
+            Digital records may be transferred through this form. Please zip files prior to uploading 
+            for transfer. Please do not transfer files via this form that contain highly sensitive or 
+            personally identifiable information (PII), such as social security numbers, credit card numbers, 
+            or information covered by right to privacy laws such as FERPA or HIPPA. For additional information, 
+            see 
+            <ul>
+               <li>IRM-012: Privacy and Confidentiality of University Information</li>
+               <li>IRM-003: Data Protection of University Information</li>
+            </ul>
+            Contact the University Archivist in advance to discuss records containing PII prior to arranging transfer.
+         </p>
+         <p>
+            <b>It is necessary to consult with the University Archivist in advance of any records transfers to 
+            ensure that the material is appropriate and in a condition for archival retention.</b> 
+            If you have any questions about this form, please contact the University Archivist.
+         </p>
+      </div>
       <div class="access-type">
-         <h3>Access the submission form</h3>
+         <h3>Access the transfer form</h3>
          <label>
             <input type="radio" name="is_uva" id="uva" value="no" checked="checked">
             I am UVA faculty, staff, or student. (You will be asked to verify your identity using 
@@ -33,8 +69,25 @@ export default {
 </script>
 
 <style scoped>
+div.contact{ 
+   color: #444;
+   padding: 10px 0 15px 0;
+   border-bottom: 1px dashed #EB5F0C;
+   margin-bottom: 20px;
+}
+div.contact p {
+   margin:0;
+   padding: 0;
+}
+td.label {
+   font-weight: bold;
+   padding-right: 10px;
+   text-align: right;
+}
 #continue-btn {
-   float: right;
+   float:right;
+   position: relative;
+   top: -28px;
 }
 a {
    color: cornflowerblue;
@@ -50,16 +103,17 @@ a:hover {
  text-decoration: underline;  
 }
 div.home {
+   position: relative;
    min-width: 1000px;
-  padding: 30px 50px;;
-  min-height: 600px;
-  background: white;
-  width: 75%;
-  margin: 0 auto;
-  border-right: 1px solid #dfdacc;
-  border-left: 1px solid #dfdacc;
-  font-weight: 400;
-  color: #333;
+   padding: 30px 50px 250px 50px;
+   min-height: 600px;
+   background: white;
+   width: 75%;
+   margin: 0 auto;
+   border-right: 1px solid #dfdacc;
+   border-left: 1px solid #dfdacc;
+   font-weight: 400;
+   color: #333;
 }
 h2 {
   margin: 0;
