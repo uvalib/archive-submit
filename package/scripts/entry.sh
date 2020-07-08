51de5696c0384069678baeb080db519fa2f9ed81
Dave Goldstein <dpg3k@virginia.edu>
2020-07-08 13:35:48 -0400

    Move to the build layer model. Update to alpine 3.12

---
+++ package/scripts/entry.sh
@@ -0,0 +1,7 @@
+# run application
+
+cd bin; ./submitsrv -upload $ARCHIVE_SUBMIT_UPLOAD_DIR -dbhost $ARCHIVE_SUBMIT_DBHOST -dbname $ARCHIVE_SUBMIT_DBNAME -dbuser $ARCHIVE_SUBMIT_DBUSER -dbpass $ARCHIVE_SUBMIT_DBPASS -smtphost $SMTP_HOST  -smtpport $SMTP_PORT -host $ARCHIVE_SUBMIT_HOST
+
+#
+# end of file
+#
